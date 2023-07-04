120672d2dbe2d4fdd88e5d681b4607059604894b
Tigran Melkonyan <timelkon@c1r4s3.42yerevan.am>
2023-07-04 23:15:58 +0400

    img_to_win

--- mandatory/game.c
+++ mandatory/game.c
@@ -6,7 +6,7 @@
 /*   By: timelkon <timelkon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 21:43:48 by timelkon          #+#    #+#             */
-/*   Updated: 2023/07/04 22:07:38 by timelkon         ###   ########.fr       */
+/*   Updated: 2023/07/04 23:12:17 by timelkon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ t_mlx	*game(t_mlx *mlx)
 {
 	int	img_widths;
 	int	img_height;
-	
-	img_height = 64;
-	img_widths = 64;
+
 	mlx->mlx = mlx_init();
 	mlx->win = mlx_new_window(mlx->mlx, 1920, 1080, "Hello world!");
 	mlx->img = mlx_xpm_file_to_image(mlx->mlx, WALL, &img_widths, &img_height);
+	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
 	mlx_hook(mlx->win, 17, 1L<<2, krest, &mlx);
 	mlx_key_hook(mlx->win, key_hook, NULL);
 	mlx_loop(mlx->mlx);
